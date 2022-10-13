cc4d330aa140d8f056bea385a033be046bd3a3e2
Isaac Sousa <isaacsousa@MacBook-de-FixPay.local>
2022-10-13 17:08:51 -0300

    Definindo constantes, construindo o plugin

--- src /recommend.php
+++ src /recommend.php
@@ -1,10 +1,29 @@
 <?php
 /**
- * Plugin Mame: Recommend
+ * @link            https://github.com/Isaac-Sousa/pluggin_Wordpress_by_hostgator
+ * @since           1.0.0
+ * @package         Recommend
+ *
+ * @wordpress-plugin
+ * Plugin Name:     Recommendation
+ * Plugin URI:      link
+ * Description:     Videos recommendations
+ * Version:         1.0.0
+ * Author:          Isaac de Sousa
+ * Author URI:      link
+ * License:         indicação/informação
+ * License URI:     link
+ * Text Domain:     Isso ai mesmo
+ * Domain Path:     link
  */
 
-add_filter( 'the_content', 'thanks' );
-
-function thanks ($content){
-    return $content.'<p><strong>Obrigado por me instalar!</strong></p>';
+if (! defined('WPINC')){
+    wp_die();
 }
+
+//Plugin Version
+if(! defined('RECOMMEND_VERSION', '1.0.0')){ //NOME DA CONSTANTE COM O NOME DO PLUGIN
+defined('RECOMMEND_VERSION', '1.0.0');
+}
+
+//Plugin Name
