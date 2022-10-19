25eba967951d210d066bc2a558eeb1ff5c595306
Isaac Sousa <isaacsousa@MacBook-de-FixPay.local>
2022-10-19 16:43:16 -0300

    _json trying to make work 100%

--- src/includes/youtube_recommendation_json.php
+++ src/includes/youtube_recommendation_json.php
@@ -3,9 +3,80 @@ if (! class_exists('Youtube_Recommendation_json')) {
 	class youtube_recommendation_json {
 		private $channel_id;
 
-     public function __construct($channel_id){
-        $this->$channel_id = $channel_id;
+     public function __construct($channel_id, $expiration = 1, $dirname, $filename){
+        $this->channel_id = $channel_id;
+		$this->expiration = $expiration;
+		$this->filename   = $filename;
+		$this->dirname    = $dirname;
+		$this->path       = $this->create_folder_path();
+
+		$ajax_action      = 'youtube_recommendation_videos';
+		add_action("wp_ajax_$ajax_action", array($this, 'write_content'));
+	    add_action("wp_ajax_nopriv_$ajax_action", array($this, 'write_content'));
+
      }
+
+	 public function write_content(){
+		 echo $this->get_content();
+		 wp_die();
+	 }
+
+     private function get_content(){
+
+		 if($this->is_expired()){
+			 echo 'Youtube';
+		  $json_content = $this->from_yt_feed();
+	      $this->save_file($json_content);
+		 }else{
+			 echo 'Cache';
+		  $json_content = $this->from_file();
+		 }
+
+	   return $json_content;
+     }
+
+	 private function is_expired(){
+		 $file_expiration_in_hours = $this->expiration;
+
+		 $json_file = $this->get_filename_full_path();
+		 $json_file_expired = (time()-filemtime($json_file)>($file_expiration_in_hours * 3600));
+
+		 return ($json_file_expired);
+	 }
+
+	 private function save_file(){
+		 $json_path = $this->get_filename_full_path();
+		 $fp        = fopen($json_path,'w');
+		 fwrite($fp,$json_path);
+		 fclose($fp);
+	 }
+
+	 private function from_file(){
+		 $json_path = $this->get_filename_full_path();
+		 $json      = file_get_contents($json_path);
+		 return $json;
+	 }
+
+	 private function get_filename_full_path(){
+      return $this->path . '/' . $this->filename;
+	 }
+
+
+
+	 private function create_folder_path(){
+		 $upload_dir = wp_upload_dir();
+		 if (! empty($upload_dir['basedir'] ) ) {
+			 $dirname = $upload_dir['basedir'] .'/'.$this->dirname;
+			 if(! file_exists($dirname ) ){
+				 wp_mkdir_p($dirname);
+
+				 return $dirname;
+			 }
+		 }
+	 }
+
+
+
      //private
      public function from_yt_feed(){
 		  $channel_id = $this->channel_id;
@@ -66,13 +137,22 @@ if (! class_exists('Youtube_Recommendation_json')) {
 
 	}
 }
+// TODO - uploads Not working
+$my = new Youtube_Recommendation_json(
+'UCFuIUoyHB12qpYa8Jpxoxow',
+1,
+RECOMMEND_PLUGIN_SLUG,
+RECOMMEND_JSON_FILENAME
+);
+
+
 
 // TODO json - Working for now
-//$my = new Youtube_Recommendation_json('UCFuIUoyHB12qpYa8Jpxoxow');
-//$json = $my->from_yt_feed();
-//var_dump($json);
+$my = new Youtube_Recommendation_json('UCFuIUoyHB12qpYa8Jpxoxow');
+$json = $my->from_yt_feed();
+var_dump($json);
 
 //TODO avatar -Not working, returning NULL value!
-$my = new Youtube_Recommendation_json('UCFuIUoyHB12qpYa8Jpxoxow');
-$avatar = $my->get_channel_avatar();
-var_dump($avatar);
+//$my = new Youtube_Recommendation_json('UCFuIUoyHB12qpYa8Jpxoxow');
+//$avatar = $my->get_channel_avatar();
+//var_dump($avatar);
