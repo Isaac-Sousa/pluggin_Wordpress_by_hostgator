89586f0c8840bc4af856ffdc4072a38d4f405387
Isaac Sousa <isaacsousa@MacBook-de-FixPay.local>
2022-10-19 13:50:48 -0300

    script rewrite some parts

--- sample/js/scripts.js
+++ sample/js/scripts.js
@@ -1,8 +1,9 @@
 const YoutubeRecommendation = {
     listCallbacks: [],
+
     async loadVideos(url){
         const postData = {
-            action: 'youtube_recommendations_videos'
+            action: 'my_youtube_recommendations_videos'
         };
         let request = jQuery.ajax({
             method: 'GET',
@@ -13,14 +14,16 @@ const YoutubeRecommendation = {
      return await request.done();
     },
 
-    buildList(jsonData, containerId, layout = 'grid',  limit = 15, lang= "en_US"){
+    buildList(jsonData, containerId, layout = "grid",  limit = 15, lang= "en_US"){
         const myData = jsonData;
-        let theList = document.createElement('div');
-        theList.className = (layout == 'list') ? 'yt-rec-list' : 'yt-rec';
+        let theList = document.createElement("div");
+
+        theList.className = layout = "list" ? "yt-rec-list" : "yt-rec";
         // pode ser também, mas no caso a cima é levado em conta que o layout pode ser grid ou lista
         // theList.className = 'yt-rec';
+
         let videos = {};
-        videos = myData.slice(0, limit)
+        if (limit != null) videos = myData.videos.slice(0, limit)
         for(let i = 0; i < videos.lenght; i++){
             theList.appendChild(
                 YoutubeRecommendation.buildListItem(
@@ -30,13 +33,14 @@ const YoutubeRecommendation = {
                 ) //YT-REC-buildlistitem
             )//appendChild
         } //for
+
         let container = document.querySelector(`#${containerId}`);
-        container.innerHTML = '';
+        container.innerHTML = "";
         container.appendChild(theList);
     },
     buildListItem(item,channel,lang){
-        const theItem = document.createElement('div');
-        theItem.className ='yt-rec-item';
+        const theItem = document.createElement("div");
+        theItem.className ="yt-rec-item";
         let viewsText = {
             pt_BR: 'visualizaões',
             en_US: 'views',
